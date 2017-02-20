29b3ee388ee08abea56c21cd8eab44dc54a5d100
Achiliku <epimetheus.hp94@gmail.com>
2017-02-20 10:21:47 +0700

    render index.html

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app     = express();
-
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/',(req, res) => {
-  res.send("Hello world");
+  res.sendFile(__dirname + '/index.html');
+  // res.send('Hello world Sonbkt');
 })
-app.listen(3000)
+// app.post('/quotes', (req, res) => {
+//   console.log(req.body);
+// })
+
+app.listen(3000);
